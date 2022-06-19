a8462cc58fa31cb5a02995307f05bc81b5361d4d
jaekyu-sim <worb1605@gmail.com>
2022-06-20 00:15:38 +0900

    jwt token 발급 Springboot 예제

---
+++ src/main/java/com/example/demo/security/SecurityService.java
@@ -0,0 +1,47 @@
+package com.example.demo.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class SecurityService {
+	private static final String SECRET_KEY = "asdfasdfasdfasdfasdffasdasdffasdasdfasdfasdfasdfasdfasdfasdfasdfasdf";
+
+	public String createToken(String subject, long expTime) {
+		if(expTime<=0)
+		{
+			throw new RuntimeException("만료 시간이 0보다 커야합니다.");
+		}
+		
+		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+		
+		byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+		
+		Key signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
+		
+		return Jwts.builder()
+				.setSubject(subject)
+				.signWith(signingKey, signatureAlgorithm)
+				.setExpiration(new Date(System.currentTimeMillis() + expTime))
+				.compact();
+	}
+	
+	public String getSubject(String token) {
+		Claims claims = Jwts.parserBuilder()
+							.setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+							.build()
+							.parseClaimsJws(token)
+							.getBody();
+		return claims.getSubject();
+	}
+}
